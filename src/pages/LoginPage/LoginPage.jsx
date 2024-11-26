dcbfcb1c16f2c2f313edb1c787f51b5b7605f5c8
TranThienTrung <xxdarkblade2906@gmail.com>
2024-11-26 19:55:37 +0700

    updatev2.5.2

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -17,7 +17,8 @@ import { userStatus } from "../../redux/userSlice";
 const LoginPage = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [isActive, setIsActive] = useState(false);
+  const [status, setStatus] = useState("nonactive");
+  const [change, setChange] = useState("nonactive");
   const { showNotification } = useContext(NotificationContext);
   const { values, handleChange, handleSubmit, touched, errors } = useFormik({
     initialValues: {
@@ -54,21 +55,271 @@ const LoginPage = () => {
         });
     },
   });
-  const toggleActive = () => {
-    setIsActive(!isActive);
-  };
 
   return (
     <>
       <Header />
       <div className="container mx-auto pt-28 pb-10 ">
-        <div className={`login-page ${isActive ? "active" : ""}`}>
-          <div className="grid grid-cols-10 gap-5 border p-10 rounded-md">
-            <div className="flex flex-col justify-center space-y-5 col-span-5 login-left">
-              <h2 className="text-3xl font-bold text-purple-800 underline uppercase text-center">
+        <div className="sm:block hidden">
+          <div className={`login-page ${status}`}>
+            <div className="grid grid-cols-10 gap-5 border p-10 rounded-md">
+              <div className="flex flex-col justify-center space-y-5 col-span-5 login-left">
+                <h2 className="text-3xl font-bold text-purple-800 underline uppercase text-center">
+                  Đăng ký
+                </h2>
+                <form className="space-y-5" onSubmit={handleSubmit}>
+                  <InputCustom
+                    name="taiKhoan"
+                    labelContent="Tài khoản"
+                    typeInput="text"
+                    value={values.taiKhoan}
+                    onChange={handleChange}
+                  />
+                  <InputCustom
+                    name="matKhau"
+                    labelContent="Mật khẩu"
+                    typeInput="password"
+                    onChange={handleChange}
+                    value={values.matKhau}
+                  />
+                  <InputCustom
+                    name="hoTen"
+                    labelContent="Họ và Tên"
+                    typeInput="text"
+                    onChange={handleChange}
+                    value={values.hoTen}
+                  />
+                  <InputCustom
+                    name="email"
+                    labelContent="Email"
+                    typeInput="text"
+                    onChange={handleChange}
+                    value={values.email}
+                  />
+                  <InputCustom
+                    name="soDT"
+                    labelContent="Số Điện Thoại"
+                    onChange={handleChange}
+                    value={values.soDT}
+                  />
+                  <div className="flex items-center text-gray-500">
+                    <p className="text-sm">
+                      Khi đăng ký bạn chấp nhận các{" "}
+                      <a
+                        href="#"
+                        className="text-purple-500 underline font-semibold"
+                      >
+                        Điều khoản
+                      </a>{" "}
+                      và{" "}
+                      <a
+                        href="#"
+                        className="text-purple-500 underline font-semibold"
+                      >
+                        Chính sách
+                      </a>{" "}
+                      của chúng tôi
+                    </p>
+                  </div>
+                  <button
+                    type="submit"
+                    className="px-5 py-3 rounded-md button-left text-center border-transparent active:scale-90"
+                  >
+                    Đăng ký
+                  </button>
+                </form>
+              </div>
+              <div className="flex flex-col justify-between space-y-5 col-span-5 login-right">
+                <div></div>
+                <div className="space-y-5">
+                  <h2 className="text-3xl uppercase font-bold text-purple-800 text-center underline">
+                    Đăng nhập
+                  </h2>
+                  <form className="space-y-5" onSubmit={handleSubmit}>
+                    <InputCustom
+                      name="taiKhoan"
+                      labelContent="Tài khoản"
+                      typeInput="text"
+                      value={values.taiKhoan}
+                      onChange={handleChange}
+                    />
+                    <InputCustom
+                      name="matKhau"
+                      labelContent="Mật khẩu"
+                      typeInput="password"
+                      onChange={handleChange}
+                      value={values.matKhau}
+                    />
+                    <div className="flex items-center gap-2">
+                      <input type="checkbox" />
+                      <span className="text-sm">Ghi nhớ đăng nhập</span>
+                    </div>
+                    <div className="text-gray-500 text-sm">
+                      <p>
+                        Quên mật khẩu ?{" "}
+                        <button className="text-purple-500 underline font-semibold">
+                          Khôi phục
+                        </button>
+                      </p>
+                    </div>
+                    <button
+                      type="submit"
+                      className="px-5 py-3 rounded-md button-left border-transparent active:scale-90"
+                    >
+                      Đăng nhập
+                    </button>
+                    <div className="text-center space-y-5 relative">
+                      <div className="">
+                        <hr />
+                        <h3 className="text-gray-400 text-sm absolute top-[-12%] left-[38%] bg-white px-2">
+                          Other login options
+                        </h3>
+                      </div>
+                      <div className="container mx-auto grid grid-cols-3 items-center w-1/3 gap-3">
+                        <img
+                          src="../../../public/icons8-facebook-48.png"
+                          alt=""
+                          className="border p-1 hover:bg-purple-200 duration-300 cursor-pointer"
+                        />
+                        <img
+                          src="../../../public/icons8-google-48.png"
+                          alt=""
+                          className="border p-1 hover:bg-purple-200 duration-300 cursor-pointer"
+                        />
+                        <img
+                          src="../../../public/icons8-apple-50.png"
+                          alt=""
+                          className="border p-1 hover:bg-purple-200 duration-300 cursor-pointer"
+                        />
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <div className="grid grid-cols-10 gap-5 p-10 rounded-md login-overlay h-full">
+              <div className="text-center col-span-5 bg-purple-100 pb-5 login-overlay-left">
+                <div className="">
+                  <Lottie
+                    animationData={animationSignIn}
+                    loop={true}
+                    style={{
+                      height: "26rem",
+                      width: "100%",
+                    }}
+                  />
+                </div>
+                <button
+                  className="x-5 py-3 rounded-md button-left text-center border-transparent active:scale-90"
+                  onClick={() => setStatus("active")}
+                >
+                  Đăng ký ?
+                </button>
+              </div>
+              <div className="text-center col-span-5 bg-purple-100 login-overlay-right">
+                <div className="">
+                  <Lottie
+                    animationData={animationSignUp}
+                    loop={true}
+                    style={{ height: "26rem", width: "100%" }}
+                  />
+                </div>
+                <button
+                  className="x-5 py-3 rounded-md button-left text-center border-transparent active:scale-90"
+                  onClick={() => setStatus("nonactive")}
+                >
+                  Đăng nhập ?
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Login Mobile */}
+        <div className="block sm:hidden">
+          <div className="flex flex-col space-y-5">
+            <div className={`grid grid-cols-2 login-mobile ${change}`}>
+              <button
+                className="login-mobile-left font-semibold rounded-md py-2 uppercase text-center"
+                onClick={() => setChange("nonactive")}
+              >
+                Đăng nhập
+              </button>
+              <button
+                className="login-mobile-right font-semibold rounded-md py-2 uppercase text-center"
+                onClick={() => setChange("active")}
+              >
+                Đăng ký
+              </button>
+            </div>
+            <div className="space-y-3 login-dang-nhap">
+              <h2 className="text-xl font-bold text-purple-800 uppercase text-center">
+                Đăng nhập
+              </h2>
+              <form className="space-y-3" onSubmit={handleSubmit}>
+                <InputCustom
+                  name="taiKhoan"
+                  labelContent="Tài khoản"
+                  typeInput="text"
+                  value={values.taiKhoan}
+                  onChange={handleChange}
+                />
+                <InputCustom
+                  name="matKhau"
+                  labelContent="Mật khẩu"
+                  typeInput="password"
+                  onChange={handleChange}
+                  value={values.matKhau}
+                />
+                <div className="flex items-center gap-2">
+                  <input type="checkbox" />
+                  <span className="text-sm">Ghi nhớ đăng nhập</span>
+                </div>
+                <div className="text-gray-500 text-sm">
+                  <p>
+                    Quên mật khẩu ?{" "}
+                    <button className="text-purple-500 underline font-semibold">
+                      Khôi phục
+                    </button>
+                  </p>
+                </div>
+                <button
+                  type="submit"
+                  className="px-5 py-3 rounded-md button-left border-transparent active:scale-90"
+                >
+                  Đăng nhập
+                </button>
+                <div className="text-center space-y-5 relative">
+                  <div className="">
+                    <hr />
+                    <h3 className="text-gray-400 text-sm absolute top-[-12%] left-[38%] bg-white px-2">
+                      Other login options
+                    </h3>
+                  </div>
+                  <div className="container mx-auto grid grid-cols-3 items-center gap-3 w-1/2">
+                    <img
+                      src="../../../public/icons8-facebook-48.png"
+                      alt=""
+                      className="border p-1 hover:bg-purple-200 duration-300 cursor-pointer"
+                    />
+                    <img
+                      src="../../../public/icons8-google-48.png"
+                      alt=""
+                      className="border p-1 hover:bg-purple-200 duration-300 cursor-pointer"
+                    />
+                    <img
+                      src="../../../public/icons8-apple-50.png"
+                      alt=""
+                      className="border p-1 hover:bg-purple-200 duration-300 cursor-pointer"
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div className="space-y-3 login-dang-ky">
+              <h2 className="text-xl font-bold text-purple-800 uppercase text-center">
                 Đăng ký
               </h2>
-              <form className="space-y-5" onSubmit={handleSubmit}>
+              <form className="space-y-3" onSubmit={handleSubmit}>
                 <InputCustom
                   name="taiKhoan"
                   labelContent="Tài khoản"
@@ -103,7 +354,25 @@ const LoginPage = () => {
                   onChange={handleChange}
                   value={values.soDT}
                 />
-
+                <div className="flex items-center text-gray-500">
+                  <p className="text-sm">
+                    Khi đăng ký bạn chấp nhận các{" "}
+                    <a
+                      href="#"
+                      className="text-purple-500 underline font-semibold"
+                    >
+                      Điều khoản
+                    </a>{" "}
+                    và{" "}
+                    <a
+                      href="#"
+                      className="text-purple-500 underline font-semibold"
+                    >
+                      Chính sách
+                    </a>{" "}
+                    của chúng tôi
+                  </p>
+                </div>
                 <button
                   type="submit"
                   className="px-5 py-3 rounded-md button-left text-center border-transparent active:scale-90"
@@ -111,108 +380,6 @@ const LoginPage = () => {
                   Đăng ký
                 </button>
               </form>
-            </div>
-            <div className="flex flex-col justify-between space-y-5 col-span-5 login-right">
-              <div></div>
-              <div className="space-y-5">
-                <h2 className="text-3xl uppercase font-bold text-purple-800 text-center underline">
-                  Đăng nhập
-                </h2>
-                <form className="space-y-5" onSubmit={handleSubmit}>
-                  <InputCustom
-                    name="taiKhoan"
-                    labelContent="Tài khoản"
-                    typeInput="text"
-                    value={values.taiKhoan}
-                    onChange={handleChange}
-                  />
-                  <InputCustom
-                    name="matKhau"
-                    labelContent="Mật khẩu"
-                    typeInput="password"
-                    onChange={handleChange}
-                    value={values.matKhau}
-                  />
-                  <div className="flex items-center gap-2">
-                    <input type="checkbox" />
-                    <span className="text-sm">Ghi nhớ đăng nhập</span>
-                  </div>
-                  <div className="text-gray-500 text-sm">
-                    <p>
-                      Quên mật khẩu ?{" "}
-                      <button className="text-purple-500 underline font-semibold">
-                        Khôi phục
-                      </button>
-                    </p>
-                  </div>
-                  <button
-                    type="submit"
-                    className="px-5 py-3 rounded-md button-left border-transparent active:scale-90"
-                  >
-                    Đăng nhập
-                  </button>
-                  <div className="text-center space-y-5">
-                    <div className="relative">
-                      <hr />
-                      <h3 className="text-gray-400 text-sm absolute top-[-1000%] left-[38%] bg-white px-2">
-                        Other login options
-                      </h3>
-                    </div>
-                    <div className="container mx-auto flex items-center justify-around w-1/3">
-                      <img
-                        src="../../../public/icons8-facebook-48.png"
-                        alt=""
-                        className="border p-1 hover:bg-purple-200 duration-300 cursor-pointer"
-                      />
-                      <img
-                        src="../../../public/icons8-google-48.png"
-                        alt=""
-                        className="border p-1 hover:bg-purple-200 duration-300 cursor-pointer"
-                      />
-                      <img
-                        src="../../../public/icons8-apple-50.png"
-                        alt=""
-                        className="border p-1 hover:bg-purple-200 duration-300 cursor-pointer"
-                      />
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-          <div className="grid grid-cols-10 gap-5 p-10 rounded-md login-overlay">
-            <div className="text-center col-span-5 bg-purple-100 pb-5 login-overlay-left">
-              <div className="">
-                <Lottie
-                  animationData={animationSignIn}
-                  loop={true}
-                  style={{
-                    height: "26rem",
-                    width: "100%",
-                  }}
-                />
-              </div>
-              <button
-                className="x-5 py-3 rounded-md button-left text-center border-transparent active:scale-90"
-                onClick={toggleActive}
-              >
-                Đăng ký ?
-              </button>
-            </div>
-            <div className="text-center col-span-5 bg-purple-100 pb-5 login-overlay-right">
-              <div className="">
-                <Lottie
-                  animationData={animationSignUp}
-                  loop={true}
-                  style={{ height: "26rem", width: "100%" }}
-                />
-              </div>
-              <button
-                className="x-5 py-3 rounded-md button-left text-center border-transparent active:scale-90"
-                onClick={toggleActive}
-              >
-                Đăng nhập ?
-              </button>
             </div>
           </div>
         </div>
